3ca805dd58acb35468d776ef0eea9d0552448cd7
caketea <51907709+caketea@users.noreply.github.com>
2019-06-27 18:16:33 +0800

    Add files via upload

---
+++ LineTrack50.c
@@ -0,0 +1,33 @@
+#pragma config(Motor,  motorB,          leftWheel,     tmotorEV3_Large, PIDControl, driveLeft, encoder)
+#pragma config(Motor,  motorC,          rightWheel,    tmotorEV3_Large, PIDControl, driveRight, encoder)
+
+#define PWR 50
+#define NPWR 0
+#define THRESHOLD 35
+#define GOSTRAIGHT 50
+
+task main()
+{
+	int leftLight = 0;
+	int rightLight = 0;
+	while (true)
+	{
+		leftLight = getColorReflected(S2);
+		rightLight = getColorReflected(S3);
+		if (leftLight > THRESHOLD && rightLight < THRESHOLD)
+		{
+			motor[rightWheel] = PWR;
+			motor[leftWheel] = NPWR;
+		}
+		else if (leftLight < THRESHOLD && rightLight > THRESHOLD)
+		{
+			motor[leftWheel] = PWR;
+			motor[rightWheel] = NPWR;
+		}
+		else
+		{
+			motor[leftWheel] = GOSTRAIGHT;
+			motor[rightWheel] = GOSTRAIGHT;
+		}
+	}
+}
